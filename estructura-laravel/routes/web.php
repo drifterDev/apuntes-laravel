644315a1c861c1335861531662efc7f7a8aad745
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-10-26 19:10:33 -0500

    Breeze

--- estructura-laravel/routes/web.php
+++ estructura-laravel/routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 // Route::get('home', function () {
 //     return view('home');
@@ -40,3 +36,20 @@ Route::controller(PageController::class)->group(function () {
     Route::get('blog', 'blog')->name('blog');
     Route::get('post/{post:slug}', 'post')->name('post');
 });
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
