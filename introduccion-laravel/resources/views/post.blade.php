84fb5b0ace5cd214eb10061eaec4fd031a766241
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-10 16:28:11 -0500

    Revisando los estilos

--- introduccion-laravel/resources/views/post.blade.php
+++ introduccion-laravel/resources/views/post.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Introducción a Laravel</title>
-</head>
-<body>
-    <h1>Post</h1>
-    <p>{{ $post }}</p>
-</body>
-</html>
+@extends('template')
+@section('content')
+<h1>Post</h1>
+<p>{{ $post }}</p>
+@endsection
---
+++ introduccion-laravel/resources/views/template.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Introducción a Laravel</title>
+    <link rel="stylesheet" href="{{ assets('assets/app.css') }}">
+</head>
+<body>
+    <div class="w-64 h-64 bg-red-800">
+        <a href="">Home</a>
+        <a href="">Blog</a>
+    </div>
+    @yield('content')
+</body>
+</html>
+
+<!--Añadir estilos-->
