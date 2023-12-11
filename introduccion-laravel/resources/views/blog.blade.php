0cb504480921cb4968395c6cf0a8c9573b26b42f
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-11 17:07:37 -0500

    Más pruebas con las migraciones y llaves foraneas

--- introduccion-laravel/resources/views/blog.blade.php
+++ introduccion-laravel/resources/views/blog.blade.php
@@ -5,6 +5,7 @@
 <p class="mt-2 hover:font-bold">
     <strong>{{ $p->id }}</strong>
     <a class="ml-5" href="{{route('post', $p->slug)}}">{{ $p->title }}</a>
+    <span class="block text-sm ml-8">{{ $p->user->name }}</span>
 </p>
 @endforeach
 <div class="mb-5"></div>
--- introduccion-laravel/resources/views/post.blade.php
+++ introduccion-laravel/resources/views/post.blade.php
@@ -1,5 +1,6 @@
 @extends('template')
 @section('content')
 <h1 class="text-xl font-bold mb-5">{{ $post->title }}</h1>
+<span class="text-sm mb-5">Author: {{$post->user->name}}</span>
 <p>{{ $post->body }}</p>
 @endsection
