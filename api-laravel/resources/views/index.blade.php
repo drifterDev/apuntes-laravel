3868ddc56962a0fe6f3c6798557c37f190909d28
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-12-17 15:16:08 -0500

    Estilos y diseño de la parte web de la api

--- api-laravel/resources/views/index.blade.php
+++ api-laravel/resources/views/index.blade.php
@@ -6,15 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Posts</title>
+    @vite('resources/css/app.css')
 </head>
 
-<body>
-    <ul>
-        @foreach ($posts as $post)
-            <li>{{ $post->title }}</li>
-        @endforeach
-    </ul>
-    {{ $posts->links() }}
+<body class="bg-gray-100 text-gray-700">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-3 my-10">
+            @foreach ($posts as $post)
+                <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                    <p class="text-xs">{{ $post->excerpt }}</p>
+                    <p class="text-xs text-right">{{ $post->published_at }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class="mb-10">
+            {{ $posts->links() }}
+        </div>
+    </div>
 </body>
 
 </html>
